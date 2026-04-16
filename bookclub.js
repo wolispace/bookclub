499abf22781a4348a67a03c587aa6b0b03cd88b2
Wallace McGee <wm@wolispace.com>
2026-04-16 20:04:15 +0930

    feat: styling schedule

--- bookclub.js
+++ bookclub.js
@@ -2,13 +2,9 @@
 let clubId = window.location.href;
 clubId = clubId.replace(window.location.origin, '').replace('/', '');
 
-console.log(window.location);
-
-
-
-function addToContents(html) {
-  let bodyElement = document.querySelector(".contents");
-  bodyElement.insertAdjacentHTML('beforeend', html);
+function addToSchedule(html) {
+  let bodyElement = document.querySelector(".schedule");
+  bodyElement.innerHTML = html;
 }
 
 function addToTitle(string) {
@@ -20,10 +16,35 @@ function getClubData($clubId) {
   return fetch(`?c=${$clubId}`);
 }
 
+function buildSchedule(clubData) {
+  let html = '';
+  Object.entries(clubData.events).forEach(([key, event]) => {
+      const date = new Date(key.slice(0,4), key.slice(4,6) - 1, key.slice(6,8));
+      const showdate = date.toLocaleString('default', { month: 'short' }); // "December", "September"
+      const { host, location, book } = event;
+      const showbook = book.replace(',', '<br>');
+      html += `<div class="event as-row">
+        <div class="datebox as-column">
+          <div class="date">${showdate}</div>
+          <div class="host">${host}</div>
+        </div>
+        <div class="titlebox as-column">
+          <div class="book">${showbook}</div>
+          <div class="location">${clubData.locations[location]}</div>
+        </div>
+        <div class="edit"><i class="fas fa-edit"></i></div>
+      </div>`;
+  });
+
+  return html;
+}
+
 // The show starts here
 document.addEventListener('DOMContentLoaded', () => {
   getClubData(clubId).then(response => response.json()).then(data => {
     addToTitle(data.name);
+    const schedule = buildSchedule(data);
+    addToSchedule(schedule);
   });
 
 });
--- index.php
+++ index.php
@@ -42,12 +42,15 @@ function outputPage($v) {
         <title>Bookclub</title>
         <meta charset='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0, interactive-widget=resizes-visual' />
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+            integrity='sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=='
+            crossorigin='anonymous' referrerpolicy='no-referrer' />
         <link rel='stylesheet' href='bookclub.css?{$v}' />
         <script src='bookclub.js?{$v}' ></script>
     </head>
     <body>
     <h1 class='title'>Bookclub</h1>
-        <div class='content'></div>
+        <div class='schedule as-column'></div>
     </body>
     </html>";
 }
