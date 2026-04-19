f98f9d935da055a6f0b4d096966d48591f5639c4
Wallace McGee <wm@wolispace.com>
2026-04-19 10:12:41 +0930

    feat: webapp and icons

--- bookclub/index.php
+++ bookclub/index.php
@@ -125,8 +125,11 @@ function outputPage($v) {
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
             integrity='sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=='
             crossorigin='anonymous' referrerpolicy='no-referrer' />
+        <link rel='icon' href='icon-192.png' type='image/png' />
         <link rel='stylesheet' href='bookclub.css?{$v}' />
         <script src='bookclub.js?{$v}' ></script>
+        <link rel='manifest' href='manifest.json' />
+        <script>navigator.serviceWorker?.register('sw.js');</script>
     </head>
     <body>
     <h1 class='clubtitle'>Bookclub</h1>
